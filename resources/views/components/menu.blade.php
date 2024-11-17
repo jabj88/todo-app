12b1d9c109f772c6af6e11384c0f23d66dc680dd
JbLobs <jorge@lobstermarketing.com>
2024-11-16 22:04:06 -0600

    fixed menu on homepage

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,5 +1,7 @@
-<nav {{ $attributes }}>
-    <li><a href="/">Home</a></li>
-    <li><a href="/report">Analitycs</a></li>
-</nav>
-<hr class="w-11/12 mx-auto border-indigo-600" />
+<div>
+    <nav {{ $attributes }}>
+        <li><a href="/">Home</a></li>
+        <li><a href="/report">Analitycs</a></li>
+    </nav>
+    <hr class="w-11/12 mx-auto border-indigo-600" />
+</div>
